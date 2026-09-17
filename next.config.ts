12b629513879740bfc180fc7bffc8f946d770d35
Poojitha Nakka <poojitha11102006@gmail.com>
2026-09-17 10:17:17 +0530

    chore: add vercel.json configuration and environment template

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
     ],
   },
 };
